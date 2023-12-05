6c0987948bc6f6bffe823dfb64c134c7c776d68f
komarc <153024228+komarc@users.noreply.github.com>
2023-12-05 22:53:46 +0100

    sikeres.js
    email2 validalas

--- js/sikeres.js
+++ js/sikeres.js
@@ -2,6 +2,9 @@ document.getElementById("sikeres").addEventListener("click",sikeres);
 
 function sikeres(){
     const email=document.getElementById("email2");
-    email.value="Sikeresen felirakoztál!";
+    if (email.value.includes("@")){
+        email.value="Sikeresen feliratkoztál!";
+    }
+    
     console.log(email);
 }
